e24bb9bf33030b0216035ee42af9d6b7718d858f
ahad <ahadkhalid117@gmail.com>
2024-09-10 20:35:54 +0530

    update main file

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main{
     public static void main(String[] args){
 System.out.println("this is java program");  
+System.out.println("this is java program 2 ");  
+System.out.println("this is java program 3");  
+System.out.println("this is java program 4");  
+System.out.println("this is java program 5");  
      }
 }
 
